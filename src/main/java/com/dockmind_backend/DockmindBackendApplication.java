08e46333e5252dfea6d65eacb2c613247d28a6a6
SantoshaC1404 <santosha1404@gmail.com>
2026-08-23 19:05:03 +0530

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/dockmind_backend/DockmindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dockmind_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockmindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockmindBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockmind_backend/DockmindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockmind_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockmindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
