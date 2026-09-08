e65bb86195f566e80a0d62e01570a3be41cfd401
SantoshaC1404 <santosha1404@gmail.com>
2026-09-08 21:44:54 +0530

    docmind updated

--- src/main/java/com/dockmind_backend/controller/DocmindController.java
+++ src/main/java/com/dockmind_backend/controller/DocmindController.java
@@ -1,5 +1,22 @@
 package com.dockmind_backend.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController()
+@RequestMapping("/api/v1/documents")
+@Tag(
+        name = "Document Management",
+        description = "Endpoints for uploading, listing and managing documents and their vectors embeddings."
+)
 public class DocmindController {
+
+    @PostMapping
+    public ResponseEntity<String> uploadDocument() {
+        // Implement the logic for uploading a document and generating its vector embeddings
+        return ResponseEntity.ok("Document uploaded successfully.");
+    }
 }
